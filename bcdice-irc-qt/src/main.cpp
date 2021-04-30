0df96b4da0de674b93d3d4eda82753e03cc7c72a
ocha <ochaochaocha3@gmail.com>
2021-04-30 23:27:20 +0900

    clang-format

--- bcdice-irc-qt/src/main.cpp
+++ bcdice-irc-qt/src/main.cpp
@@ -1,9 +1,9 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QTranslator>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
   QApplication a(argc, argv);
   MainWindow w;
   w.show();
