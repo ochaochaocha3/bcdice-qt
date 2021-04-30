122502d5ada260585e4d2023d6ea0d3716617c3a
ocha <ochaochaocha3@gmail.com>
2021-04-30 23:59:34 +0900

    レイアウト、翻訳作り始め

--- bcdice-irc-qt/src/main.cpp
+++ bcdice-irc-qt/src/main.cpp
@@ -2,10 +2,21 @@
 
 #include <QApplication>
 #include <QTranslator>
+#include <iostream>
 
 int main(int argc, char* argv[]) {
-  QApplication a(argc, argv);
+  QApplication a{argc, argv};
+
+  QTranslator translator;
+  QString translationFile{"translations/bcdice-irc-qt_ja_JP"};
+  if (translator.load(translationFile)) {
+    a.installTranslator(&translator);
+  } else {
+    qWarning() << "Could not load" << translationFile;
+  }
+
   MainWindow w;
   w.show();
+
   return a.exec();
 }
--- bcdice-irc-qt/translations/bcdice-irc-qt_ja_JP.ts
+++ bcdice-irc-qt/translations/bcdice-irc-qt_ja_JP.ts
@@ -1,3 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ja_JP"></TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.ui" line="14"/>
+        <source>mainWindow-title</source>
+        <translation>BCDice IRC</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="24"/>
+        <source>versionGroupBox-title</source>
+        <translation>バージョン情報</translation>
+    </message>
+</context>
+</TS>
